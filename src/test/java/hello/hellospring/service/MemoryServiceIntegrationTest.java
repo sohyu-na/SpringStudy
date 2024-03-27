8fa8ae3e968fa27743e0d7b2c079dff46978649b
SoHyunA <solim73@naver.com>
2024-03-27 15:52:58 +0900

    스프링 데이터 jpa

--- src/test/java/hello/hellospring/service/MemoryServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemoryServiceIntegrationTest.java
@@ -22,7 +22,7 @@ class MemberServiceIntegrationTest {
     public void 회원가입() throws Exception {
         //Given
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring2");
         //When
         Long saveId = memberService.join(member);
         //Then
